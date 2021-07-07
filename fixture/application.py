246d71f015bb58811f40274e00a8bbd50c10a426
mihail <tarakanov.m.a@mail.ru>
2021-07-07 15:34:48 +0300

    added new project for mantisbt

---
+++ fixture/application.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from fixture.session import SessionHelper
+
+
+class Application:
+
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized %s" % browser)
+        # self.wd.implicitly_wait(1)
+        self.session = SessionHelper(self)
+        self.base_url = base_url
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def open_home_page(self):
+        wd = self.wd
+        if not (wd.current_url == self.base_url
+           and len(wd.find_elements_by_link_text("Last name")) > 0):
+            wd.get(self.base_url)
+
+    def destroy(self):
+        self.wd.quit()
---
+++ fixture/common.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import random
+import string
+
+
+def wait_for(wd, element, value, timeout=5):
+    wait = WebDriverWait(wd, timeout)
+    return wait.until(EC.presence_of_element_located((element, value)))
+
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(1, maxlen))])
